52d5c665ca8e8fcc71d936f476da34cd81f7d49f
Bapugouda Gulabal <38274563+bapugouda-gulabal@users.noreply.github.com>
2021-05-09 11:30:11 +0530

    Add files via upload

---
+++ src/main/java/TypewiseAlert/BatteryCharacter.java
@@ -0,0 +1,16 @@
+package TypewiseAlert;
+
+public class BatteryCharacter {
+    private CoolingType coolingType;
+    private String brand;
+	public CoolingType getCoolingType() {
+		return coolingType;
+	}
+	public void setCoolingType(CoolingType coolingType) {
+		this.coolingType = coolingType;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+  }
